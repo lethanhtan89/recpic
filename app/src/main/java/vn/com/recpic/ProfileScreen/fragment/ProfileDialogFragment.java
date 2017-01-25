f65a3a5db3d49343c5e4fc91c51ab91f7b390ea1
lethanhtan89 <lethanhtan89@gmail.com>
2017-01-25 14:02:13 +0700

    export dialog

--- app/src/main/java/vn/com/recpic/ProfileScreen/fragment/ProfileDialogFragment.java
+++ app/src/main/java/vn/com/recpic/ProfileScreen/fragment/ProfileDialogFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -34,6 +35,7 @@ public class ProfileDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dialog_profile, container, false);
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         init(view);
         return view;
     }
