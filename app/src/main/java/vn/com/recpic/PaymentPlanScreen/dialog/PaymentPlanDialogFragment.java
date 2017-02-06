4405236f4cfc42fdabeabbe014e6b95b461ce424
lethanhtan89 <lethanhtan89@gmail.com>
2017-02-06 09:58:14 +0700

    set no title payment dialog

--- app/src/main/java/vn/com/recpic/PaymentPlanScreen/dialog/PaymentPlanDialogFragment.java
+++ app/src/main/java/vn/com/recpic/PaymentPlanScreen/dialog/PaymentPlanDialogFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.LinearLayout;
 
 import vn.com.recpic.R;
